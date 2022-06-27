a022b3c6c5a78640aca18190135745f494e8b3cb
hj <mmjh9258@gmail.com>
2022-06-27 14:41:10 +0900

    Day11 phoenix live view license example

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule LiveViewStudio.MixProject do
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:tailwind, "~> 0.1"}
+      {:tailwind, "~> 0.1", runtime: Mix.env() == :dev}
     ]
   end
 
